dd64e245037871c51786a3b55fbec621177d5297
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-04 14:43:32 +0300

    feat: add styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,66 +6,85 @@ import './App.css'
 
 
 const App = () => {
-  const {register, handleSubmit, formState: {errors}, reset} = useForm()
+  const {register, handleSubmit, formState: {errors}} = useForm()
   const onSubmit = handleSubmit((data) => console.log(data))
   const floors = useFill(3,27)
   const meetingsRoom = useFill(1,10)
   
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>
-          Выбор башни
-          <select id='tower' {...register("tower")}>
-          <option value="A">
-              A
-          </option>
-          <option value="B">
-              B
-          </option>
-        </select>
-        </label>
+    <div className='body'>
+      <div className='container'>
+        <h1 className='title'>
+          Забронировать переговорную
+        </h1>
+        <form onSubmit={onSubmit} className='form'>
+          <div className='selects'>
+            <div>
+              <label className='label'>
+                <span className='description'>
+                  Выбор башни
+                </span>
+                <select id='tower' {...register("tower")}>
+                <option value="A">
+                    A
+                </option>
+                <option value="B">
+                    B
+                </option>
+              </select>
+              </label>
+            </div>
+            <div>
+              <label className='label'>
+                <span className='description'>
+                  Выбор этажа
+                </span>
+                <select {...register("floor")}>
+                    {floors.map(floor => <option key={floor} value={floor}>{floor}</option>)}
+                </select>
+              </label>
+            </div>
+            <div>
+              <label className='label'>
+                <span className='description'>
+                  Выбор номера переговорной
+                </span>
+                <select {...register("meetingRoomNumber")}>
+                    {meetingsRoom.map(room => <option key={room} value={room}>{room}</option>)}
+                </select>
+              </label>
+            </div>
+          </div>
+          <div className='block'>
+            <label className='label'>
+              <span className='description'>
+                Дата
+              </span>
+              <input className='input' type='date' {...register("date", {required: "Дата обязательна"})}/>
+              {errors.date && <span>{errors.date.message}</span>}
+            </label>
+          </div>
+          <div className='block'>
+            <label className='label'>
+              <span className='description'>
+                Комментарий
+              </span>
+              <textarea className='textarea' defaultValue="" {...register("comment")}/>
+            </label>
+          </div>
+          <div className='block'>
+            <div className='buttons'>
+              <button type='submit' className='button'>
+                Отправить
+              </button>
+              <button type='reset' className='button'>
+                Очистить
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>
-          Выбор этажа
-          <select {...register("floor")}>
-              {floors.map(floor => <option key={floor} value={floor}>{floor}</option>)}
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Выбор номера переговорной
-          <select {...register("meetingRoomNumber")}>
-              {meetingsRoom.map(room => <option key={room} value={room}>{room}</option>)}
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Дата
-          <input type='date' {...register("date", {required: "Дата обязательна"})}/>
-          {errors.date && <span>{errors.date.message}</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Комментарий
-          <textarea {...register("comment")}/>
-        </label>
-      </div>
-      <div>
-        <div>
-          <button type='submit'>
-            Отправить
-          </button>
-          <button onClick={() => reset({})}>
-            Очистить
-          </button>
-        </div>
-      </div>
-    </form>
+    </div>
   )
 }
 
